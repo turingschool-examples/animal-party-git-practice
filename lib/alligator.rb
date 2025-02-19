6453b7839f89938887761d97a970d749ff8f0c27
Maia Bard <Maiakbard@gmail.com>
2025-02-19 14:45:21 -0700

    Add alligator class

--- lib/alligator.rb
+++ lib/alligator.rb
@@ -1,3 +1,10 @@
 class Alligator
+    attr_reader :name, :pen_number, :taxon_class
+
+    def initialize(name, pen_number)
+        @name = name
+        @pen_number = pen_number
+        @taxon_class = :reptile
+    end
 
 end
