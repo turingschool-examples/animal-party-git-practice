9779238bb00b38819b19c15bbcd8e316ff819d6b
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-02-19 15:28:45 -0600

    Added a monkey class

--- lib/monkey.rb
+++ lib/monkey.rb
@@ -1,3 +1,10 @@
 class Monkey
-
+  attr_reader :name,
+              :pen_number,
+              :taxon_class
+  def initialize(name, pen_number)
+    @name = name
+    @pen_number = pen_number
+    @taxon_class = :mammal
+  end
 end
